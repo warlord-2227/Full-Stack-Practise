494b02abb1b14d6aadae5fb2958248969218aada
warlord-2227 <setu.iitb@gmail.com>
2023-12-13 18:51:23 +0530

    todo test

--- week_1/assignments-master/01-js/hard/todo-list.js
+++ week_1/assignments-master/01-js/hard/todo-list.js
@@ -11,7 +11,57 @@
 */
 
 class Todo {
+  constructor()
+  {
+    this.result = [];
+  }
+add(task)
+{
+  this.result.push(task)
+}
+remove(index)
+{
+  if (index >= this.result.length)
+  {
+    console.log("Wrong index and we can't remove any todo associated with it");
+    return this.result
+  }
+  else
+  {
+   this.result.splice(index,1);
+   //return this.result 
+  }
+}
+update(index,item)
+{
+  if (index < this.result.length)
+  {
+   this.result.splice(index,1,item);
+  
+  }
+  //return this.result
+}
+getAll()
+{
+  return this.result
+}
+get(index)
+{
+  if (index < 0 || index >= this.result.length )
+  {
+    console.log("Invalid index");
+    return null;
+  } 
+  return this.result[index]
+  // let default = -1 ;
+  // for (let i=0;i<this.result.length;i++)
+  // {
 
+  // }
+}
+clear(){
+  this.result = [];
+}
 }
 
 module.exports = Todo;
